62e973b87a8b7cc94ff8541f05fd11b272a73be7
Thomaz <thomaz.zandonotto@gmail.com>
2017-10-03 18:10:05 -0300

    npm morgan e npm bcrypt adicionados. Arquivo app.js adicionado.

---
+++ app.js
@@ -0,0 +1,28 @@
+// Creates the references for the external modules;
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express(); // Creates a reference variable to the express package;
+var morgan = require('morgan');
+var bodyParser = require('body-parser')
+
+// Creates the references for the application modules;
+var routes = require('./api/routes/index');
+var init = require('./database/init');
+
+// Middlewares;
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api', routes);
+
+// App config
+const port = process.env.PORT || 8080; // Defines the port we're using to access the application;
+const dbUrl = 'mongodb://localhost:27017/usersDb'; // Defines the url we're using to connect to the database;
+
+//Server port setup;
+app.listen(port, function() {
+  console.log('running the server on port ' + port);
+});
+
+//Database setup
+init.dbInit(dbUrl);
