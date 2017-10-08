208243a83dc9f7a4a36a4baec84b05e68c583043
Thomaz <thomaz.zandonotto@gmail.com>
2017-10-08 01:07:24 -0300

    Reorganização dos diretórios da aplicação

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 var router = express.Router();
-var appRoutes = require('./api/routes/index')(router);
+var appRoutes = require('./server/routes/index')(router);
 var path = require('path');
 
 // Creates the references for the application modules;
-var init = require('./database/init');
+var init = require('./server/database/init');
 
 // Middlewares;
 app.use(morgan('dev'));
@@ -22,12 +22,13 @@ app.use('/api', appRoutes);
 const port = process.env.PORT || 8080; // Defines the port we're using to access the application;
 const dbUrl = 'mongodb://localhost:27017/usersDb'; // Defines the url we're using to connect to the database;
 
-//Database setup
+// Database setup
 init.dbInit(dbUrl);
 
-// app.get('*', function(req, res){
-//   res.sendFile(path.join(__dirname + '/public/index.html'))
-// })
+// Indica a view index.html como padrão para conexão via localhost
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/views/index.html'))
+})
 
 //Server port setup;
 app.listen(port, function() {
