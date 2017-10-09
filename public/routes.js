136d47187889901fb885753398974edd54be0645
Thomaz <thomaz.zandonotto@gmail.com>
2017-10-08 23:22:41 -0300

    rotina de redirecionamento 'otherwise' adicionada às rotas de navegação via angular-route.

--- public/routes.js
+++ public/routes.js
@@ -1,13 +1,27 @@
-angular.module('appRoutes', ['ngRoute'])
+var appRoutes = angular.module('appRoutes', ['ngRoute'])
 
-.config(function($routeProvider,$locationProvider) {
-  $locationProvider.hashPrefix('');
+appRoutes.config(function($routeProvider,$locationProvider) {
+
   $locationProvider.html5Mode(true);
+
   $routeProvider
+
   .when('/', {
     templateUrl: 'views/pages/home.html'
   })
+
   .when('/about', {
     templateUrl: 'views/pages/about.html'
-  });
+  })
+
+  .when('/register', {
+    templateUrl: 'views/pages/register.html'
+  })
+
+  .when('/projects', {
+    templateUrl: 'views/pages/projects.html'
+  })
+
+  .otherwise ({ redirectTo: '/' });
+
 })
