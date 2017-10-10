884abffbe4c953ae4c039ec7024dac863728e06a
thomazmz <thomaz.zandonotto@gmail.com>
2017-10-10 15:19:57 -0300

    Implementa o controlador para leitura dos projetos.

--- public/controllers/project.js
+++ public/controllers/project.js
@@ -4,16 +4,29 @@ projectControllers.controller('newProject', function($scope, $http){
   console.log('testing projectController')
   $scope.onSubmit = newProject;
   function newProject(){
-    $http.post('http://localhost:8080/api/project', $scope.newProject).then(function(data) {
+    $http.post('http://localhost:8080/api/projects', $scope.newProject).then(function(data) {
+      console.log(data)
     });
   }
 })
 
-userControllers.controller('getAllProjects', function($scope, $http) {
+projectControllers.controller('getAllProjects', function($scope, $http) {
   getAllProjects();
   function getAllProjects(){
-    $http.get('http://localhost:8080/api/project').then(function(projects){
+    $http.get('http://localhost:8080/api/projects').then(function(projects){
       $scope.projects = projects.data;
-    })
+    });
   }
-});
+})
+
+projectControllers.controller('getProjectById', function($scope, $http, $routeParams) {
+  $scope.getProjectById = function (){
+    console.log('entrou aqui')
+    var id = $routeParams.id;
+    console.log(id);
+    $http.get('/api/projects/'+id).then(function(data) {
+      $scope.project = data.data;
+      console.log(data.data)
+    });
+  }
+})
