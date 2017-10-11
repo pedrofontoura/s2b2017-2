c4633c42ae08095cc6c18dac1d3db16c434e4f0c
Thomaz <thomaz.zandonotto@gmail.com>
2017-10-11 20:45:41 -0300

    Remoção do código de teste dos controladores do arquivo project.js

--- public/controllers/project.js
+++ public/controllers/project.js
@@ -3,7 +3,6 @@ var projectControllers = angular.module('projectControllers', []);
 
 // Função que adiciona um novo projeto [POST]
 projectControllers.controller('newProject', function($scope, $http){
-  console.log('testing projectController')
   $scope.onSubmit = newProject;
   function newProject(){
     $http.post('http://localhost:8080/api/projects', $scope.newProject).then(function(data) {
@@ -22,15 +21,12 @@ projectControllers.controller('getAllProjects', function($scope, $http) {
   }
 })
 
-// Função que busca um projeto específico a partir do seu id [GET] 
+// Função que busca um projeto específico a partir do seu id [GET]
 projectControllers.controller('getProjectById', function($scope, $http, $routeParams) {
   $scope.getProjectById = function (){
-    console.log('entrou aqui')
     var id = $routeParams.id;
-    console.log(id);
     $http.get('/api/projects/'+id).then(function(data) {
       $scope.project = data.data;
-      console.log(data.data)
     });
   }
 })
