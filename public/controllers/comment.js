23f64aeadb1e1e70c3009438220ff54f6da8e041
Thomaz <thomaz.zandonotto@gmail.com>
2017-10-13 06:27:42 -0300

    Solução de bugs na função deleteComment e createComment

--- public/controllers/comment.js
+++ public/controllers/comment.js
@@ -4,10 +4,14 @@ var commentControllers = angular.module('commentControllers', [])
 // Função que solicita adição de um novo comentário [POST]
 commentControllers.controller('newComment',function($scope,$http,$routeParams) {
   $scope.onSubmit = newComment
-  function newComment(){
-    var newComment = { id : $routeParams.id , text : $scope.text };
+    function newComment(){
+    var newComment = { id : $routeParams.id , text : $scope.comment.text };
     $http.post('http://localhost:8080/api/comments', newComment).then(function(data) {
       $scope.getProjectComments();
+      $scope.resetForm = function() {
+        $scope.comment = angular.copy($scope.master);
+      };
+      $scope.resetForm();
     });
   }
 });
@@ -16,18 +20,24 @@ commentControllers.controller('newComment',function($scope,$http,$routeParams) {
 commentControllers.controller('getProjectComments', function($scope,$http,$routeParams) {
   $scope.getProjectComments = function(){
     var id = $routeParams.id;
-    $http.get('http://localhost:8080/api/comments/'+id, $routeParams.id).then(function(comments) {
+    $http.get('http://localhost:8080/api/projects/comments/'+id).then(function(comments) {
       $scope.comments = comments.data;
     });
   }
 });
 
-//Função que solicita delete de um comentário vinculado a um project [DELETE]
+// Função que solicita delete de um comentário vinculado a um project [DELETE]
 commentControllers.controller('deleteComment', function($http,$scope) {
   $scope.deleteComment = function(commentId){
-    console.log(commentId)
     $http.delete('http://localhost:8080/api/comments/'+commentId).then(function(comments) {
       $scope.getProjectComments();
     });
   }
+});
+
+// Função que busca um comentário específico a partir do seu id [GET]
+commentControllers.controller('editComment', function($http,$scope) {
+  $(".myBtn").click(function(){
+         $("#editModal").modal("toggle");
+     });
 });
