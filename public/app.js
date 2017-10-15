da23f58a50a66bc338e1a6a359ab8026f0636b02
Thomaz <thomaz.zandonotto@gmail.com>
2017-10-14 23:38:07 -0300

    Alteração na view user.html

--- public/app.js
+++ public/app.js
@@ -1,2 +1,22 @@
 // Criação do módulo principal e suas dependências
 angular.module('s2bApp',['appRoutes', 'userControllers', 'projectControllers', 'commentControllers'])
+
+function openTab(evt, tabName, btnName) {
+  var tabWraps, tabBtns;
+  tabWraps = document.getElementsByClassName("tabWrap");
+  for (let i = 0; i < tabWraps.length; i++) {
+    if(tabName == tabWraps[i].id) {
+      tabWraps[i].classList.add("tabWrapCurrent");
+    } else {
+      tabWraps[i].classList.remove("tabWrapCurrent");
+    }
+  }
+  tabBtns = document.getElementsByClassName("profileNavdBtn");
+  for (let i = 0; i < tabBtns.length; i++) {
+    if(btnName == tabBtns[i].id) {
+      tabBtns[i].classList.add("profileNavdBtnCurrent");
+    } else {
+      tabBtns[i].classList.remove("profileNavdBtnCurrent");
+    }
+  }
+}
