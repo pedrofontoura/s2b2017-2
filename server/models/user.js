c21589eae20f90ce895adb3cba662b154b78d027
S2B JavaScript <pedrofontoura93@gmail.com>
2017-10-16 15:22:28 -0200

    Pequena correção na model user.js

--- server/models/user.js
+++ server/models/user.js
@@ -13,19 +13,19 @@ var UserSchema = new Schema({
 // Função que define a senha do usuário
 // salt: string random única para cada usuário
 // hash: senha + salt => encript
-userSchema.methods.setPassword = function (password) {
+UserSchema.methods.setPassword = function (password) {
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
 // Faz o hash da senha e combina com o hash armazenado para validação
-userSchema.methods.validPassword = function (password) {
+UserSchema.methods.validPassword = function (password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
   return this.hash === hash;
 };
 
 // Gerando o token de login
-userSchema.methods.generateJwt = function () {
+UserSchema.methods.generateJwt = function () {
   var expiry = new Date();
   expiry.setDate(expiry.getDate() + 7);
 
